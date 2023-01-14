468f69d17c644c7c60f9c483adf00fef033ef4ba
Saurav <saurav24021998@gmail.com>
2023-01-14 22:38:17 +0530

    Saurav Test

---
+++ array/addOne/saurav.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class saurav{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList<String>arr = new ArrayList<>();
+        arr.add("sairav");
+        arr.add("Pritam");
+    }
+}
