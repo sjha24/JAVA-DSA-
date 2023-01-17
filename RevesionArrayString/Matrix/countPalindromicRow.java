1b81eade706c6f43d540387424ac9f9e995d0d6c
Saurav <saurav24021998@gmail.com>
2023-01-17 12:37:56 +0530

    searchINSortedMat.java

--- RevesionArrayString/Matrix/countPalindromicRow.java
+++ RevesionArrayString/Matrix/countPalindromicRow.java
@@ -6,26 +6,30 @@ public class countPalindromicRow {
         int rows = scn.nextInt();
         int cols = scn.nextInt();
         int mat[][] = new int[rows][cols];
-        for(int r = 0; r<rows; r++){
-            for(int c = 0;c<cols; c++){
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
                 mat[r][c] = scn.nextInt();
             }
         }
         int count = 0;
-        for(int row = 0; row<rows; row++){
-            if(isPalindromicRow(mat, rows, cols, row)==true)count++;
+        for (int row = 0; row < rows; row++) {
+            if (isPalindromicRow(mat, rows, cols, row) == true)
+                count++;
         }
         System.out.println(count);
     }
-    public static boolean isPalindromicRow(int mat[][], int rows, int cols, int row){
-       int left = 0, right = cols -1;
-        for(int r = row; r<rows;r++){
-            while(left<right){
-                if(mat[r][left] != mat[r][right])return false;
-                left++;right--;
-            }
+
+    public static boolean isPalindromicRow(int mat[][], int rows, int cols, int row) {
+        int left = 0, right = cols - 1;
+
+        while (left < right) {
+            if (mat[row][left] != mat[row][right])
+                return false;
+            left++;
+            right--;
         }
+
         return true;
     }
-   
+
 }
